e83061714b351be9133d6d62c008246afbf593f5
spiridonov-oa <spiridonov.oa@gmail.com>
2016-07-21 20:44:21 +0300

    Change label for text_account

--- www/catalog/language/russian/common/header.php
+++ www/catalog/language/russian/common/header.php
@@ -6,7 +6,7 @@ $_['text_shopping_cart']  = 'Корзина покупок';
 $_['text_search']   = 'Поиск';
 $_['text_welcome']  = '<a href="%s">Войти</a> | <a href="%s">зарегистрироваться</a>'; //@language@ spiridonov.oa@gmail.com
 $_['text_logged']   = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
-$_['text_account']  = 'Постоянный покупатель';
+$_['text_account']  = 'Личный Кабинет';
 $_['text_checkout'] = 'Оформление заказа';
 $_['text_logout']   = 'Выйти'; //@language@ spiridonov.oa@gmail.com
 ?>
